b415130b635e274dc953e1962f7d79d0b8403806
Eloi RM <eloimailmaynad@gmail.com>
2024-10-30 16:52:14 +0100

    fix progress bar

--- game.js
+++ game.js
@@ -73,6 +73,7 @@ nextButton.addEventListener('click', ()=>{
 replayButton.addEventListener('click', () =>{
     currentQuestionIndex = 0
     score = 0
+    progressBar.value = 0
     answersBox.style.display = "inline-block"
     nextButton.style.display = "inline-block"
     questionImage.style.display = "inline-block"
