483b67acebce386abf88d56fee730658bd6da4fb
Dorothee <braud.dorothee@gmail.com>
2024-10-30 15:58:05 +0100

    ajouter les images

--- questions.js
+++ questions.js
@@ -4,26 +4,31 @@ questions:
 [
     {
         text: "Comment s'appellent les bonbons spécialités de Cambrai ?",
+        image: "image1.jpg",
         options: ["les mignardises", "les bêtises", "les arlequins", "les cambrons"],
         correct_answer: "les bêtises"
     },
     {
         text: "Quel est le plat emblématique des français le jour de noël ?",
+        image: "image2.png",
         options: ["la dinde rôtie", "les pizzas", "les brocolis", "les patates douces"],
         correct_answer: "la dinde rôtie"
     },
     {
         text: "D'ou vient le beurre salé ?",
+        image: "image3.png",
         options: ["de la tête d'un illuminé breton", "de régions qui souhaitaient concerver le beurre plus longtemps", "de Clermont-Ferrant", "de vaches gavées au sel"],
         correct_answer: "de régions qui souhaitaient concerver le beurre plus longtemps"
     },
     {
         text: "Laquelle de ces patisseries n'utilise pas d'alcool dans sa préparation ?",
+        image: "image4.jpg",
         options: ["le gâteau nantais", "le baba au rhum", "le kouign-amann", "le tiramisu"],
         correct_answer: "le kouign-amann"
     },
     {
         text: "Laquelle des suggestions suivantes à la bonne orthographe ?",
+        image: "image5.jpg",
         options: ["coun ya-man", "kouign-amann", "queen hamam", "cuign-amann"],
         correct_answer: "kouign-amann"
     },
