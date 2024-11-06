e825c405775bdbcdab6ec38bcb7740813c4bd68c
Dorothee <braud.dorothee@gmail.com>
2024-11-06 10:09:50 +0100

    progress bar fixed

--- game.js
+++ game.js
@@ -69,7 +69,7 @@ function loadQuiz(quizName){ //charge un quiz choisit
     questionImage.style.display = "inline-block" 
     replayButton.style.display = "none"
     timerElement.style.display = "block"
-    progressBar.style.display = "block"
+    progressBar.style.display = "inline-block"
     //selectionne le quiz choisit
     currentQuiz = quizVersion[quizName]
     // if(quizName=="Alsace"){
